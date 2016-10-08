4cbd65286008a80531462ce21cd2247249df0deb
Adrian Cheong <adriancheong@gic.com.sg>
2016-10-08 22:52:35 +0800

    initial project creation and push

--- src/SecuritiesPopulater/Program.cs
+++ src/SecuritiesPopulater/Program.cs
@@ -1,14 +1,51 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace SecuritiesPopulater
 {
     public class Program
     {
+        private static readonly string REDIS_ALIAS = "REDIS";
+        private static readonly string REDIS_ADDRESS_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_ADDR";
+        private static readonly string REDIS_PORT_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_PORT";
+        private static readonly int NUMBER_OF_SECURITIES_TO_CREATE = 1000;
+
         public static void Main(string[] args)
         {
+            initAndPopulateRedisStore();
+        }
+
+        private static void initAndPopulateRedisStore()
+        {
+            var configurationOptions = new ConfigurationOptions
+            {
+                EndPoints =
+                {
+                    { Environment.GetEnvironmentVariable(REDIS_ADDRESS_ENV_PROPERTY_KEY), int.Parse(Environment.GetEnvironmentVariable(REDIS_PORT_ENV_PROPERTY_KEY)) }
+                }
+            };
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(configurationOptions);
+
+            IDatabase db = redis.GetDatabase();
+            IList<Security> securities = SecuritiesGenerator.GenerateRandomSecurities(NUMBER_OF_SECURITIES_TO_CREATE);
+            foreach (Security security in securities)
+            {
+                db.HashSet(security.SecurityId, GenerateRedisHash<Security>(security));
+                Console.WriteLine("Storing Security {0} to Redis", security.SecurityId);
+            }
+        }
+
+        private static HashEntry[] GenerateRedisHash<T>(T obj)
+        {
+            var props = typeof(T).GetProperties();
+            var hash = new HashEntry[props.Count()];
+            for (int i = 0; i < props.Count(); i++)
+                hash[i] = new HashEntry(props[i].Name, props[i].GetValue(obj).ToString());
+            return hash;
         }
     }
 }
---
+++ src/SecuritiesPopulater/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecuritiesPopulater
+{
+    public class Security
+    {
+        public string SecurityId { get; set; }
+        public string Owner { get; set; }
+        public SecurityType Type { get; set; }
+        public double Valuation { get; set; }
+        public string Currency { get; set; }
+        public string CountryOfOrigin { get; set; }
+    }
+
+    public enum SecurityType
+    {
+        Debt,
+        Equity,
+        Hybrid
+    }
+}

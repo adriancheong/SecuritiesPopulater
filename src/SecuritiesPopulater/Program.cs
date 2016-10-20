2a113d8ba60d0d76a74a983c4ade0bfd01ab3ed8
Adrian Cheong <adriancheong@gic.com.sg>
2016-10-20 16:42:14 +0800

    Refactor to have plug and play stores

--- src/SecuritiesPopulater/Program.cs
+++ src/SecuritiesPopulater/Program.cs
@@ -1,17 +1,7 @@
-﻿using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Reflection;
-
-namespace SecuritiesPopulater
+﻿namespace SecuritiesPopulater
 {
     public class Program
     {
-        private static readonly string REDIS_ALIAS = "REDIS";
-        private static readonly string REDIS_ADDRESS_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_ADDR";
-        private static readonly string REDIS_PORT_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_PORT";
         private static readonly int NUMBER_OF_SECURITIES_TO_CREATE = 100000;
 
         public static void Main(string[] args)
@@ -24,39 +14,8 @@ namespace SecuritiesPopulater
                 if (args.Length > 1)
                     int.TryParse(args[1], out startingSecurityId);
             }
-            initAndPopulateRedisStore(numberOfSecurities, startingSecurityId);
-        }
-
-        private static void initAndPopulateRedisStore(int numberOfSecurities, int startingSecurityId)
-        {
-            Console.WriteLine("Creating REDIS Endpoints");
-            Console.WriteLine("REDIS_ADDRESS_ENV_PROPERTY_KEY = {0}", Environment.GetEnvironmentVariable(REDIS_ADDRESS_ENV_PROPERTY_KEY));
-            Console.WriteLine("REDIS_PORT_ENV_PROPERTY_KEY = {0}", Environment.GetEnvironmentVariable(REDIS_PORT_ENV_PROPERTY_KEY));
-            var configurationOptions = new ConfigurationOptions
-            {
-                EndPoints =
-                {
-                    { Environment.GetEnvironmentVariable(REDIS_ADDRESS_ENV_PROPERTY_KEY), int.Parse(Environment.GetEnvironmentVariable(REDIS_PORT_ENV_PROPERTY_KEY)) }
-                }
-            };
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(configurationOptions);
-
-            IDatabase db = redis.GetDatabase();
-            IList<Security> securities = SecuritiesGenerator.GenerateRandomSecurities(numberOfSecurities, startingSecurityId);
-            foreach (Security security in securities)
-            {
-                db.HashSet(security.SecurityId, GenerateRedisHash<Security>(security));
-                Console.WriteLine("Storing Security {0} to Redis", security.SecurityId);
-            }
-        }
-
-        private static HashEntry[] GenerateRedisHash<T>(T obj)
-        {
-            var props = typeof(T).GetProperties();
-            var hash = new HashEntry[props.Count()];
-            for (int i = 0; i < props.Count(); i++)
-                hash[i] = new HashEntry(props[i].Name, props[i].GetValue(obj).ToString());
-            return hash;
+            IStore store = new RedisStore();
+            store.Populate(numberOfSecurities, startingSecurityId);
         }
     }
 }
---
+++ src/SecuritiesPopulater/RedisStore.cs
@@ -0,0 +1,48 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SecuritiesPopulater
+{
+    public class RedisStore : IStore
+    {
+        private static readonly string REDIS_ALIAS = "REDIS";
+        private static readonly string REDIS_ADDRESS_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_ADDR";
+        private static readonly string REDIS_PORT_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_PORT";
+
+        public void Populate(int numberOfSecurities, int startingSecurityId)
+        {
+            Console.WriteLine("Creating REDIS Endpoints");
+            Console.WriteLine("REDIS_ADDRESS_ENV_PROPERTY_KEY = {0}", Environment.GetEnvironmentVariable(REDIS_ADDRESS_ENV_PROPERTY_KEY));
+            Console.WriteLine("REDIS_PORT_ENV_PROPERTY_KEY = {0}", Environment.GetEnvironmentVariable(REDIS_PORT_ENV_PROPERTY_KEY));
+            var configurationOptions = new ConfigurationOptions
+            {
+                EndPoints =
+                {
+                    { Environment.GetEnvironmentVariable(REDIS_ADDRESS_ENV_PROPERTY_KEY), int.Parse(Environment.GetEnvironmentVariable(REDIS_PORT_ENV_PROPERTY_KEY)) }
+                }
+            };
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(configurationOptions);
+
+            IDatabase db = redis.GetDatabase();
+            IList<Security> securities = SecuritiesGenerator.GenerateRandomSecurities(numberOfSecurities, startingSecurityId);
+            foreach (Security security in securities)
+            {
+                db.HashSet(security.SecurityId, GenerateRedisHash<Security>(security));
+                Console.WriteLine("Storing Security {0} to Redis", security.SecurityId);
+            }
+        }
+
+        private static HashEntry[] GenerateRedisHash<T>(T obj)
+        {
+            var props = typeof(T).GetProperties();
+            var hash = new HashEntry[props.Count()];
+            for (int i = 0; i < props.Count(); i++)
+                hash[i] = new HashEntry(props[i].Name, props[i].GetValue(obj).ToString());
+            return hash;
+        }
+    }
+}
